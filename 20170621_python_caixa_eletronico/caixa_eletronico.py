a469b00eab67cb63bdb39e6236fb063d54097665
gilsondev <me@gilsondev.in>
2017-06-21 23:32:09 -0300

    Gilson e Samuel

--- 20170621_python_caixa_eletronico/caixa_eletronico.py
+++ 20170621_python_caixa_eletronico/caixa_eletronico.py
@@ -3,17 +3,16 @@ Notas disponíveis de R$ 100,00; R$ 50,00; R$ 20,00 e R$ 10,00
 """
 
 
+def maior_nota_possivel(valor):
+    for nota in (100, 50, 20, 10):
+        if valor // nota > 0:
+            return nota
+
+
 def saque(valor):
-    if valor in (40, 30, 10):
-        notas = []
-        max_nota = 20
-        while valor > 0:
-            qtd, valor = divmod(valor, max_nota)
-            notas += [max_nota] * qtd
-            max_nota = 10
-        return notas
-
-
-    if valor == 50:
-        return [50]
-
+    notas = []
+    while valor > 0:
+        max_nota = maior_nota_possivel(valor)
+        qtd, valor = divmod(valor, max_nota)
+        notas += [max_nota] * qtd
+    return notas
