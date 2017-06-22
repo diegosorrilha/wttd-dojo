c608fce6a456b178837d054f831cf0b1a00e5345
Diego Sorrilha <diego@x4start.com>
2017-06-21 22:48:01 -0300

    Diego e Henrique

--- 20170621_python_caixa_eletronico/caixa_eletronico.py
+++ 20170621_python_caixa_eletronico/caixa_eletronico.py
@@ -4,9 +4,15 @@ Notas disponíveis de R$ 100,00; R$ 50,00; R$ 20,00 e R$ 10,00
 
 
 def saque(valor):
+    # if valor in (40, 30):
+
 
     if valor == 40:
-        return [20, 20]
+        notas = []
+        max_nota = 20
+        res = 40 // max_nota
+        notas+=[max_nota] * res
+        return notas
 
     if valor == 30:
         return [20, 10]
