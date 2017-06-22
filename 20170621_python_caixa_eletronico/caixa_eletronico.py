981c0ee6e8f51a4a81582f21f1a1b7c7367d11ef
Vicente E. R. Marçal <vicente.marcal@gmail.com>
2017-06-21 22:29:13 -0300

    River e Diego

--- 20170621_python_caixa_eletronico/caixa_eletronico.py
+++ 20170621_python_caixa_eletronico/caixa_eletronico.py
@@ -2,11 +2,17 @@
 Notas disponíveis de R$ 100,00; R$ 50,00; R$ 20,00 e R$ 10,00
 """
 
+
 def saque(valor):
-    if valor == 10:
-        return [10]
 
     if valor == 40:
         return [20, 20]
 
-    return [20, 10]
+    if valor == 30:
+        return [20, 10]
+
+    if valor == 50:
+        return [50]
+
+    if valor == 10:
+        return [10]
